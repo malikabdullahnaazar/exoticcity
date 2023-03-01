ecc8233f381d05d26c62c03aa3d68603603ca4a3
Malik Abdullah <malikabdullah130037@example.com>
2023-03-01 11:51:30 +0500

    creating folder and browser routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Index from "./components/Index";
+import Brands from "./components/Brands";
+import MyAccount from "./components/My_Account";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/brands' element={<Brands />} />
+          <Route path='/my-account' element={<MyAccount />} />
+          <Route path='/order-tracking' element={<MyAccount />} />
+        </Routes>
       </header>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/My_Account.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const My_Account = () => {
+  return (
+    <div>My_Account</div>
+  )
+}
+
+export default My_Account
