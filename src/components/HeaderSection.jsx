b61d01d23c15336e8525c43244f54bdb229833f8
Malik Abdullah <malikabdullah130037@example.com>
2023-03-03 12:48:24 +0500

    nav bar 03/03/2023

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -4,27 +4,32 @@ import "./css/HeaderSection.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MDBCol } from "mdbreact";
 import { IoPersonOutline } from 'react-icons/io5';
+import { MdOutlineArrowDropDown } from "react-icons/md";
 const HeaderSection = () => {
-  
+
   return (
     <div className="logo-design">
       <div>
         <img src={exocitcity} alt="Exocitcity" width="170vw" />
         <div>Online Shopping Center</div>
       </div>
-      <div className="site-location">
-        <a href="/">
-          <span className="location-description">Your Location</span>
-          <div className="current-location">Select a Location</div>
-        </a>
+      <div className="site-location d-flex">
+        <div className="col">
+            <div>Your Location</div>
+            <div>Select a Location</div>
+          
+        </div>
+        <div className="col-auto">
+          <MdOutlineArrowDropDown size={38} className="drop-icon current-location" />
+        </div>
       </div>
       <div className="site-location1">
-        <MDBCol md="6">
+        <MDBCol md="6" size="15" xl="12">
           <input className="form-control " type="text" placeholder="Search for products" aria-label="Search" />
         </MDBCol>
       </div>
-      <div >
-        <IoPersonOutline size={35}/>
+      <div className="person-icon">
+        <IoPersonOutline size={35} />
       </div>
 
 
