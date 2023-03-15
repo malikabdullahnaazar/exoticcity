7ee853ffd51a2a1c89a05b76acce433c82a5e567
Malik Abdullah <malikabdullah130037@example.com>
2023-03-15 11:02:32 +0500

    commit

--- src/components/shope/ShopRight.jsx
+++ src/components/shope/ShopRight.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/shopright.css';
 const shopRight = () => {
   return (
-    <div>shopRight</div>
+    <div className='main'>
+      <div className="container">
+        <div className="background-image">
+          <div className="d-flex align-items-center justify-content-center ">
+            <div className="align-items-center justify-content-center my-5">
+
+              <p>Delivered to Your Home</p>
+              <h2>Organic Meals Prepared</h2>
+              <p>Fully prepared & delivered nationwide</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex align-items-start  text-black backgroung-custom m-3 p-4">
+        
+      </div>
+
+    </div>
   )
 }
 
