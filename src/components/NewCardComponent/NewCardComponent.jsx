fd5338d912ddaad275109aea872bc24db440147d
Shahvaiz-Ahmed <91520897+Shahvaiz-Ahmed@users.noreply.github.com>
2023-04-27 12:19:33 +0500

    cart

--- src/components/NewCardComponent/NewCardComponent.jsx
+++ src/components/NewCardComponent/NewCardComponent.jsx
@@ -83,7 +83,7 @@ function NewCardComponent(props) {
               <span style={{ fontSize: 'smaller' }} > {props.quantity > 0 ? 'IN STOCK' : 'OUT OF STOCK'}</span>
             </Typography>
             <Typography variant="h6" color="#d51243">
-              <strong> € {props.price?(props.price).toFixed(3):("0.00")}</strong>
+              <strong> € {props.price?(props.price).toFixed(3):("0.00 m")}</strong>
             </Typography>
           </CardContent>
 
