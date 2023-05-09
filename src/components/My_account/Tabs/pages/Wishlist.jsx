6898d89448fb72b2c7b5e407352a75740a1aa1c2
Malik Abdullah <malikabdullah13037@gmail.com>
2023-05-09 12:55:20 +0500

    fixed the total cart price in any where

--- src/components/My_account/Tabs/pages/Wishlist.jsx
+++ src/components/My_account/Tabs/pages/Wishlist.jsx
@@ -1,42 +1,100 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../../../UserContext';
+import { Link } from "react-router-dom";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+const Wishlist = () => {
+    const { wishlist,setwishlist,login,setCartItem,CartItem} = useContext(UserContext);
+    let Selected = [];
+    const handleRemoveFromWishlist = (index) => {
+      const updatedWishlist = [...wishlist];
+      updatedWishlist.splice(index, 1);
+      setwishlist(updatedWishlist);
+    };
+    const addAlltoCart = () => {
+      const updatedCart = [...CartItem];
+      wishlist.forEach(item => {
+        const cartItemIndex = updatedCart.findIndex(cartItem => cartItem.product?cartItem.product.itemNo===item.itemNo: cartItem.itemNo === item.itemNo);
+        if (cartItemIndex !== -1) {
+          updatedCart[cartItemIndex].quantity += 1;
+        } else {
+          updatedCart.push({
+            ...item,
+            quantity: 1
+          });
+        }
+      });
+      setCartItem(updatedCart);
+    };
+    function addSelectedtoCart (){
+      const updatedCart = [...CartItem];
+      Selected.forEach(item => {
+        const cartItemIndex = updatedCart.findIndex(cartItem => cartItem.itemNo === item.itemNo);
+        if (cartItemIndex !== -1) {
+          updatedCart[cartItemIndex].quantity += 1;
+        } else {
+          updatedCart.push({
+            ...item,
+            quantity: 1
+          });
+        }
+      });
+      setCartItem(updatedCart);
+    }
+  
+  // Define an empty array to hold the selected items
+  let selectedItems = [];
+  
+  // Define a function to handle the checkbox click event
+  function handleCheckbox(item) {
+    const checkbox = document.getElementById("selectid");
+    if (checkbox) {
+      // Add the item to the selected items array
+      selectedItems.push(item);
+    } else {
+      // Remove the item from the selected items array
+      selectedItems = selectedItems.filter((i) => i.itemNo !== item.itemNo);
+    }
+  
+    console.log(selectedItems);
+  }
+  return (
+    <div className='container border'>
+    <table class="table table-bordered">
+<thead>
+<tr>
+  <th scope="col"><input type="checkbox" name="" id="selectAll" /></th>
+  <th scope="col"></th>
+  <th scope="col"></th>
+  <th scope="col">Product Name</th>
+  <th scope="col">Unit Price</th>
+  <th scope="col">Date Added</th>
+  <th scope="col">Stock Status</th>
+  <th scope="col"></th>
+</tr>
+</thead>
+<tbody>
+{wishlist?wishlist.map((item, index) => (
+<tr key={index}>
+<td><input type="checkbox" name="" id="selectid" onClick={handleCheckbox(item)}/></td>
+<td className='text-start'><AiOutlineCloseCircle onClick={handleRemoveFromWishlist}/></td>
+<td className='text-start'><img src={item.picture} alt="" height={150}/></td>
+<td className='text-start'>{item.product?(item.product.Description):item.Description}</td>
+<td className='text-start'>{login?(item.product?(item.product.unitprice.toFixed(3)):item.price):0}</td>
+<td className='text-start'>{item.date.toString()}</td>
+<td className='text-start'>{item.product?(item.product.minimumquantity>0?("IN Stock"):'Out Of Stock '):item.minimumquantity>0 ?("IN Stock"):'Out Of Stock '}</td>
+<td className='text-start'> <Link to={`/product/${item.product?(item.product.itemNo):item.itemNo}`} className="text-decoration-none text-black"><button className='btn btn-sm btn-rounded btn-purple' style={{width:"8vw",height:"5vh" }}>Read more</button></Link></td>
+</tr>
+)):""}
 
-
-const WishList = () => {
-    return (
-        <div className='container border'>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col"><input type="checkbox" name="" id="selectAll" /></th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col">Product Name</th>
-                        <th scope="col">Unit Price</th>
-                        <th scope="col">Date Added</th>
-                        <th scope="col">Stock Status</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type="checkbox" name="" id="selectid" /></td>
-                        <td className='text-start'>X</td>
-                        <td className='text-start'> <img src="" alt="" /> </td>
-                        <td className='text-start'>Product</td>
-                        <td className='text-start'>1200</td>
-                        <td className='text-start'>March 22,2023</td>
-                        <td className='text-start'>In Stock</td>
-                        <td className='text-start'><button className='btn btn-sm btn-rounded btn-purple ' style={{ width: "8vw", height: "5vh" }}>Read more</button></td>
-                    </tr>
-
-                </tbody>
-            </table>
-            <div className="container-fluid d-flex mx-5  flex-row">
-                <button className='btn-purple hover   ' >Add Selected to Cart</button>
-                <button className='btn-purple mx-1' >Add All to Cart</button>
-            </div>
-        </div>
-    )
+</tbody>
+</table>
+<div className="container-fluid d-flex mx-5  flex-row">
+<button className='btn-purple hover' onClick={addSelectedtoCart} >Add Selected to Cart</button>
+<button className='btn-purple mx-1' onClick={addAlltoCart} >Add All to Cart</button>
+</div>
+    </div>
+  )
 }
 
-export default WishList
+export default Wishlist
+
--- src/components/Right/BestSeller/BestSeller.jsx
+++ src/components/Right/BestSeller/BestSeller.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import CardComponent from '../CardComponent/CardComponent';
 import './BestSeller.css';
 import pic1 from '../../../Static/Images/pic1.png';
@@ -6,8 +6,9 @@ import pic2 from '../../../Static/Images/pic2.jpg';
 import pic3 from '../../../Static/Images/pic3.jpg';
 import pic4 from '../../../Static/Images/pic4.jpg';
 import ViewAll from '../ViewAllComponent/ViewAll';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import Carousel from 'react-bootstrap/Carousel';
+import { UserContext } from '../../../UserContext';
 function BestSeller() {
 
   const [index, setIndex] = useState(0);
@@ -15,49 +16,82 @@ function BestSeller() {
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-
+  const { filterprice } = useContext(UserContext);
   return (
     <div className="bestSeller">
-        <div className="bestSellerHeading">
+      <div className="bestSellerHeading">
         <div className="bestSellerText">
-            <h3>BEST SELLERS</h3>
-            <p>Do not miss the current offers until the end of March.</p>
+          <h3>BEST SELLERS</h3>
+          <p>Do not miss the current offers until the end of March.</p>
         </div>
-            <ViewAll/>
+        <ViewAll />
+      </div>
+      <div className="bestSellerCrousel">
+        <div className="prevButton">
+          <p>&#8249;</p>
         </div>
-        <div className="bestSellerCrousel">
-            <div className="prevButton">
-            <p>&#8249;</p>
-            </div>
-            <div className="bestSellerCards">
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item  >
-        <div className="crouselItem">
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      </div>
-      </Carousel.Item>
-      <Carousel.Item  >
-        <div className="crouselItem">
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      </div>
-      </Carousel.Item>
-      <Carousel.Item  >
-        <div className="crouselItem">
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      <Link to="Product" className='Link'><CardComponent pic={pic1}/></Link>
-      </div>
-      </Carousel.Item>
-    </Carousel>
-            </div>
-            <div className="nextButton">
-            <p>&#8250;</p>
-            </div>
+        <div className="bestSellerCards">
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <div className="crouselItem">
+                {filterprice ? filterprice.slice(0, 3).map(product => (
+                  <Link to="Product" className='Link'>
+                    <CardComponent
+                      Brand={product.SystemId}
+                      No={product.No}
+                      itemNo={product.itemNo}
+                      price={product.unitprice}
+                      Description={product.Description}
+                      weight={product.weight}
+                      quantity={product.quantity}
+                      picture={product.itemSystemid}
+                    />
+                  </Link>
+                )) : ''}
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="crouselItem">
+                {filterprice ? filterprice.slice(3, 6).map(product => (
+                  <Link to="Product" className='Link'>
+                    <CardComponent
+                      Brand={product.SystemId}
+                      No={product.No}
+                      itemNo={product.itemNo}
+                      price={product.unitprice}
+                      Description={product.Description}
+                      weight={product.weight}
+                      quantity={product.quantity}
+                      picture={product.itemSystemid}
+                    />
+                  </Link>
+                )) : ''}
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="crouselItem">
+                {filterprice ? filterprice.slice(6, 9).map(product => (
+                  <Link to="Product" className='Link'>
+                    <CardComponent
+                      Brand={product.SystemId}
+                      No={product.No}
+                      itemNo={product.itemNo}
+                      price={product.unitprice}
+                      Description={product.Description}
+                      weight={product.weight}
+                      quantity={product.quantity}
+                      picture={product.itemSystemid}
+                    />
+                  </Link>
+                )) : ''}
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </div>
+        <div className="nextButton">
+          <p>&#8250;</p>
+        </div>
+      </div>
     </div>
   )
 }
