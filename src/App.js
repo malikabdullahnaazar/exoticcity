b347eb5667be7569300430b684089593d500127b
Malik Abdullah <malikabdullah13037@gmail.com>
2023-05-16 08:47:05 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+
+import { useEffect ,useState} from 'react';
 import axios from 'axios'
 function App() {
  
-  
+  const [accessToken, setAccessToken] = useState('');
   useEffect(() => {
-    const tokenEndpoint = "https://cors-anywhere.herokuapp.com/https://login.microsoftonline.com/7c885fa6-8571-4c76-9e28-8e51744cf57a/oauth2/v2.0/token";
+    const tokenEndpoint = "https://login.microsoftonline.com/7c885fa6-8571-4c76-9e28-8e51744cf57a/oauth2/v2.0/token";
     const clientId = "0598e72a-da3f-4f95-bd93-7a27d0797e68";
     const clientSecret = "CUv8Q~nKj3RshRdV~yoyA1zuTino9hPM8xCFDbGh";
     const resource = "https://api.businesscentral.dynamics.com";
-    const scope = "api://0598e72a-da3f-4f95-bd93-7a27d0797e68/exoticScope";
-  
+    const scope = "https://api.businesscentral.dynamics.com/.default";
+
     axios.post(tokenEndpoint, {
       grant_type: 'client_credentials',
       client_id: clientId,
-      resource: resource,
       client_secret: clientSecret,
-      scope: scope
+      scope: scope,
+      resource: resource,
+      
     }, {
       headers: {
         'Access-Control-Allow-Origin': '*'
       }
     }).then((response) => {
       const accessToken = response.data.access_token;
+      setAccessToken(accessToken);
       console.log(accessToken);
     }).catch((error) => {
       console.error(error);
     });
   }, []);
+  useEffect(() => {
+    
+  
+    return () => {
+      console.log(accessToken);
+    }
+  }, [accessToken])
+  
   
   return (
     <div className="App">
