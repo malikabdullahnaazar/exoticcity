f3c0038676fade2c77009ff037729d7df8f84c4d
Shahvaiz-Ahmed <91520897+Shahvaiz-Ahmed@users.noreply.github.com>
2023-05-26 09:28:44 +0500

    ,,

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,13 +7,22 @@ import Footer from "./Footer/Footer";
 
 const Layout = ({ children }) => {
     return (
-        <div>
+        <div style={{
+            position: 'relative',
+
+        }} >
             < HeadderBanner />
             < HeadderBanner2 />
             <HeaderSection />
             <NavBar />
             {children}
+            <div style={{
+            marginTop: '10rem',
+            marginBottom: 0,
+
+        }} >
             <Footer />
+            </div>
         </div>
     );
 };
