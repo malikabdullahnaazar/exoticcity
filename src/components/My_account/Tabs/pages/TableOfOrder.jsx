f023d90bf9d143d5e0fea4866f00c8337f0ad4c7
Malik Abdullah <malikabdullah13037@gmail.com>
2023-05-30 12:07:03 +0500

    done

--- src/components/My_account/Tabs/pages/TableOfOrder.jsx
+++ src/components/My_account/Tabs/pages/TableOfOrder.jsx
@@ -1,59 +1,68 @@
-import React,{useContext} from 'react'
+import React from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table';
-import {Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import "../../Css/table.css"
 import { UserContext } from '../../../../UserContext';
 import axios from 'axios';
-import "../../Css/table.css"
 const TableOfOrder = () => {
-  const {OderDetails} =useContext(UserContext)
-  const loadOrderHistory=()=>{
-    axios.get('https://api.businesscentral.dynamics.com/v2.0/7c885fa6-8571-4c76-9e28-8e51744cf57a/Sandbox18/api/v2.0/companies(f03f6225-081c-ec11-bb77-000d3abcd65f)/salesOrders?$expand=salesOrderLines')
-    .then(response => {
-      // Handle the response data
-      console.log(response.data);
-    })
-    .catch(error => {
-      // Handle the error
-      console.error(error);
-    });
+  const { accessToken, userDetails } = useContext(UserContext)
+  const [custNo, setcustNo] = useState()
+  const [dataAPI, setdataAPI] = useState(null)
+  useEffect(() => {
 
-  }
-  console.log(loadOrderHistory);
+    return () => {
+      axios.get('https://api.businesscentral.dynamics.com/v2.0/7c885fa6-8571-4c76-9e28-8e51744cf57a/Sandbox18/api/v2.0/companies(f03f6225-081c-ec11-bb77-000d3abcd65f)/salesOrders?$expand=salesOrderLines',
+        {
+
+          headers: {
+
+
+
+            "Authorization": `Bearer ${accessToken}`
+
+          }
+
+        }).then(async (res) => {
+          let a = await JSON.parse(JSON.parse(userDetails));
+          setdataAPI(res.data.value);
+          setcustNo(a.No);
+        })
+    }
+  }, [custNo, dataAPI, accessToken, userDetails])
   return (
     <div>
-    <Table responsive>
-  <thead>
-    <tr>
-    <th>Order</th>
-    <th >Date</th>
-    <th >Status</th>
-    <th >Total</th>
-    <th >Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    {OderDetails?<tr>
-      <td ><Link>{OderDetails.number}</Link> </td>
-      <td>	{OderDetails.orderDate}</td>
-      <td>	{OderDetails.status}</td>
-      {
-        OderDetails.salesOrderLines.map((i)=>{
-          return <><td>	€{i.totalAmountIncludingTax
-          } for {i.qunatity} items</td>
-          <td>	
-            <button className='custom-button'>View</button>
-            <button className='custom-button'>Re-Order</button>
-            <button className='custom-button'>Place same Order</button>
-            </td></>
-        })
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>Order</th>
+            <th >Date</th>
+            <th >Status</th>
+            <th >Total</th>
+            <th >Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+              dataAPI ? dataAPI.filter((item) => {
+                return custNo ? item.customerNumber.includes(custNo) : ''
+              }).map((item) => {
+                return <tr>
+                  <td ><Link>#{item.number}</Link> </td>
+                  <td>    {item.orderDate}</td>
+                  <td>    {item.status}</td>
+                  <td>    €{item.totalAmountIncludingTax}</td>
+                </tr>
+              }) : <></>
+            }
 
-      }
-      
-    </tr>:<></>}
-   
-  </tbody>
-</Table>
-</div>
+
+
+
+          
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
